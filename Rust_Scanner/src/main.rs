ec93f38546f9bc8942299a16533ab80a1db99921
Ayodub <66164546+Ayodub@users.noreply.github.com>
2022-03-07 16:46:16 +1100

    Add files via upload

---
+++ Rust_Scanner/src/main.rs
@@ -0,0 +1,31 @@
+use rayon::prelude::*;
+
+fn main() -> Result<()> {
+	
+	let pool = rayon::ThreadPoolBuilder::new()
+		.num_threads(256)
+		.build()
+		.unwrap();
+
+	pool.install(|| {
+		let scan_result: Vec<Subdomain> = subdomains::enumerate(&http_client, target)
+			.unwrap()
+			.into_part_iter()
+			.map(ports::scan_ports)
+			.collect();
+
+		for subdomain in scan_result {
+			println!("{}", &subdomain.domain);
+			for port in &subdomain.open_ports {
+				println!("    {}", port.port);
+			}
+
+			println!("");
+		}
+	});
+	
+}
+
+
+
+
